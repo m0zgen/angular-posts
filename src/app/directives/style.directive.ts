7473aa4c0a283af8b4ce8070895297819471e43a
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2020-08-13 14:59:14 +0600

    add dynamic style decorator

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -7,6 +7,9 @@ export class StyleDirective {
 
   @Input('appStyle') color = 'blue';
   @Input() dStyles: {border?: string, fontWeight?: string};
+
+  // More decorator variant
+  @HostBinding('style.color') elColor = null;
 
   constructor(private el: ElementRef, private r: Renderer2) {
 
@@ -19,13 +22,15 @@ export class StyleDirective {
 
   /* tslint:disable:typedef */
   @HostListener('mouseenter') onEnter() {
-    this.r.setStyle(this.el.nativeElement, 'color', this.color);
+    // this.r.setStyle(this.el.nativeElement, 'color', this.color);
+    this.elColor = this.color
     this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border);
     this.r.setStyle(this.el.nativeElement, 'fontWeight', this.dStyles.fontWeight);
   }
   //
   @HostListener('mouseleave') onLeave() {
-    this.r.setStyle(this.el.nativeElement, 'color', null);
+    // this.r.setStyle(this.el.nativeElement, 'color', null);
+    this.elColor = null
     this.r.setStyle(this.el.nativeElement, 'border', null);
     this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
   }
